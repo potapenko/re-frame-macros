7eac2e3c6934115098667d0efa0374ba2f7ce9bf
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2018-07-15 19:45:41 +0200

    stable

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame-macros "0.1.15-SNAPSHOT"
+(defproject re-frame-macros "0.1.15"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
